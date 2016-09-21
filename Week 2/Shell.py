b759e808000ece0273505790b2150c251c2f5459
ssedrick <ssedrick1@gmail.com>
2016-09-21 00:13:13 -0600

    Added load code for different datasets

--- Week 2/Shell.py
+++ Week 2/Shell.py
@@ -1,12 +1,12 @@
 import sys
-import csv
+import pandas as pd
 import random
 import numpy as np
 from sklearn import datasets
 from kNNClassifier import KNNClassifier
 
 
-class IrisData:
+class SourceData:
     def __init__(self):
         self.data = []
         self.target = []
@@ -29,11 +29,36 @@ def get_split_percentage():
     return percent / 100
 
 
+def load_car_dataset():
+    df = pd.read_csv('cars.csv')
+    df = df.replace('vhigh', 4).replace('high', 3).replace('med', 2).replace('low', 1)
+    df = df.replace('5more', 6).replace('more', 5)
+    df = df.replace('small', 1).replace('med', 2).replace('big', 3)
+    df = df.replace('unacc', 1).replace('acc', 2).replace('good', 3).replace('vgood', 4)
+    car = df.values
+    data = SourceData()
+    data.data, data.target = car[:, :6], car[:, 6]
+    data.data, data.target = data.data.astype(int), data.target.astype(int)
+    return data
+
+
+def get_dataset():
+    i = input("Which dataset should I load? [1] Iris, [2] Cars, [3] Breast Cancer ")
+    if i == '1' or i.lower == 'iris':
+        return datasets.load_iris()
+
+    if i == '2' or i.lower == 'cars':
+        return load_car_dataset()
+
+    if i == '3' or i.lower in 'breast cancer':
+        return datasets.load_breast_cancer()
+
+
 def main(args):
-    iris = datasets.load_iris()
+    source = get_dataset()
 
     # Shuffle the data and target together
-    shuffled = list(zip(iris.data, iris.target))
+    shuffled = list(zip(source.data, source.target))
 
     random.shuffle(shuffled)
 

8ece5826861d9ebe38b74a304da91b996305089d
ssedrick <ssedrick1@gmail.com>
2016-09-20 23:41:07 -0600

    Finished coding kNN Classifier.

--- Week 2/Shell.py
+++ Week 2/Shell.py
@@ -3,7 +3,7 @@ import csv
 import random
 import numpy as np
 from sklearn import datasets
-from hardCoded import HardCoded
+from kNNClassifier import KNNClassifier
 
 
 class IrisData:
@@ -29,44 +29,8 @@ def get_split_percentage():
     return percent / 100
 
 
-def load_csv():
-    names = np.array(datasets.load_iris().target_names).tolist()
-    if len(sys.argv) == 1:
-        source = input("Where is the iris data csv? ")
-    else:
-        source = sys.argv[1]
-
-    imported_data = IrisData()
-
-    with open(source, newline='\n') as csvfile:
-        csv_data = csv.reader(csvfile, delimiter=',')
-        for row in csv_data:
-            imported_data.data
-            imported_data.data.append(row[0:4])
-            imported_data.target
-            full_name = row[4]
-            name = full_name.split('-')[1]
-            imported_data.target.append(names.index(name))
-
-    # Turn lists into arrays
-    imported_data.data = np.asarray(imported_data.data)
-    imported_data.target = np.asarray(imported_data.target)
-    return imported_data
-
-
-def load_data():
-    while True:
-        flag = input("Load from [1]CSV or [2]sklearn: ")
-        if flag == "1" or flag == "CSV" or flag == "csv":
-            return load_csv()
-        elif flag == "2" or flag == "sklearn":
-            return datasets.load_iris()
-        else:
-            print("Sorry, I didn't understand your input. Try inputting a 1 or a 2.")
-
-
 def main(args):
-    iris = load_data()
+    iris = datasets.load_iris()
 
     # Shuffle the data and target together
     shuffled = list(zip(iris.data, iris.target))
@@ -89,8 +53,10 @@ def main(args):
     training_target = target_array[:data_split]
     test_target = target_array[data_split:]
 
+    k = int(input("How many neighbors should we use? "))
+
     # Test with our classifier
-    tester = HardCoded()
+    tester = KNNClassifier(k)
     tester.train(training_data, training_target)
 
     result = tester.predict(test_data)
--- Week 2/kNNClassifier.py
+++ Week 2/kNNClassifier.py
@@ -1,3 +1,33 @@
+import numpy as np
+
+
 class KNNClassifier(object):
-    def __init__(self):
-        pass
+    def __init__(self, neighbors):
+        self.k = neighbors
+        self.data = []
+        self.targets = []
+
+    def train(self, data, targets):
+        self.data = data
+        self.targets = targets
+
+    def predict(self, inputs):
+        n_inputs = np.shape(inputs)[0]
+        closest = np.zeros(n_inputs)
+
+        for n in range(n_inputs):
+            # Compute Distances
+            distances = np.sum((self.data-inputs[n])**2, axis=1)
+
+            indices = np.argsort(distances, axis=0)
+
+            classes = np.unique(self.targets[indices[:self.k]])
+            if len(classes) == 1:
+                closest[n] = np.unique(classes)
+            else:
+                counts = np.zeros(max(classes) + 1)
+                for i in range(self.k):
+                    counts[self.targets[indices[i]]] += 1
+                closest[n] = np.max(counts)
+
+        return closest
